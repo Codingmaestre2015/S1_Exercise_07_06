b0e94a63eebb878f2bbaa37303732a573933dd6a
Patrick Clawson <patrick.clawson@west-mec.org>
2018-04-02 17:20:10 -0700

    Final Commit
    CVC number validation and Luhn function to validate credit card combos

--- co_payment.js
+++ co_payment.js
@@ -66,6 +66,7 @@ window.addEventListener("load", function() {
     document.getElementById("cardNumber").oninput = validateNumber;
     document.getElementById("expMonth").onchange = validateMonth;
     document.getElementById("expYear").onchange = validateYear;
+    document.getElementById("cvc").oninput = validateCVC;
 });
 
 function runSubmit() {
@@ -74,6 +75,21 @@ function runSubmit() {
     validateNumber();
     validateMonth();
     validateYear();
+    validateCVC();
+}
+
+function validateCVC() {
+    var cardCVC = document.getElementById("cvc");
+    var creditCard = document.querySelector('input[name="credit"]:checked').value;
+    if (cardCVC.validity.valueMissing) {
+        cardCVC.setCustomValidity("Enter your CVC number");
+    } else if ((creditCard === "amex") && (/^\d{4}$/.test(cardCVC.value) === false)) {
+        cardCVC.setCustomValidity("Enter a 4-digit CVC number");
+    } else if ((creditCard !== "amex") && (/^\d{3}$/.test(cardCVC.value) === false)) {
+        cardCVC.setCustomValidity("Enter a 3-digit CVC number");
+    } else {
+        cardCVC.setCustomValidity("");
+    }
 }
 
 function validateMonth() {
@@ -118,7 +134,35 @@ function validateNumber() {
         cardNumber.setCustomValidity("Enter your card number");
     } else if (cardNumber.validity.patternMismatch) {
         cardNumber.setCustomValidity("Enter a valid card number");
+    } else if (luhn(cardNumber.value) === false) {
+        cardNumber.setCustomValidity("Enter a legitimate card number");
     } else {
         cardNumber.setCustomValidity("");
     }
+}
+
+function sumDigits(numStr) {
+    var digitTotal = 0;
+    for (var i = 0; i < numStr.length; i++) {
+        digitTotal += parseInt(numStr.charAt(i));
+    }
+    return digitTotal;
+}
+
+function luhn(idNum) {
+    var string1 = "";
+    var string2 = "";
+
+    // Retrieve the odd numbered digits
+    for (var i = idNum.length - 1; i >= 0; i -= 2) {
+        string1 += idNum.charAt(i);
+    }
+
+    // Retrieve the even numbered digits
+    for (var i = idNum.length - 2; i >= 0; i -= 2) {
+        string2 += idNum.charAt(i);
+    }
+
+    // Return whether the sum of the digits is divisible by 10
+    return sumDigits(string1 + string2) % 10 === 0;
 }
